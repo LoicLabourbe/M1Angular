2dee1fc54cf21e468d48ef49894e7e47eddaae77
Loic Labourbe <loic.labourbe@etu.univ-orleans.fr>
2018-01-05 19:12:29 +0100

    css couleur

--- src/app/const/availableColors.ts
+++ src/app/const/availableColors.ts
@@ -1,20 +1,20 @@
 import {Color} from '../Classes/color';
 
 export const COLORS: Color[]=[
-  {libelle:"rouge",htmlCode:"#FF0000"},
-  {libelle:"marron",htmlCode:"#A52A2A"},
-  {libelle:"orange",htmlCode:"#FFA500"},
-  {libelle:"jaune",htmlCode:"#FFFF00"},
-  {libelle:"vert",htmlCode:"#00FF00"},
-  {libelle:"olive",htmlCode:"#808000"},
-  {libelle:"bleue",htmlCode:"#0000FF"},
-  {libelle:"bleue foncé",htmlCode:"#0000A0"},
-  {libelle:"violet",htmlCode:"#800080"},
-  {libelle:"magenta",htmlCode:"#FF00FF"},
-  {libelle:"mauve ancien",htmlCode:"#FAB0FF"},
-  {libelle:"bleue ciel",htmlCode:"#CCEEFF"},
-  {libelle:"blanc",htmlCode:"#FFFFFF"},
-  {libelle:"argent",htmlCode:"#C0C0C0"},
-  {libelle:"gris",htmlCode:"#808080"},
-  {libelle:"noir",htmlCode:"#000000"}
+  {libelle:"Rouge",htmlCode:"#FF0000"},
+  {libelle:"Marron",htmlCode:"#A52A2A"},
+  {libelle:"Orange",htmlCode:"#FFA500"},
+  {libelle:"Jaune",htmlCode:"#FFFF00"},
+  {libelle:"Vert",htmlCode:"#00FF00"},
+  {libelle:"Olive",htmlCode:"#808000"},
+  {libelle:"Bleue",htmlCode:"#0000FF"},
+  {libelle:"Bleue foncé",htmlCode:"#0000A0"},
+  {libelle:"Violet",htmlCode:"#800080"},
+  {libelle:"Magenta",htmlCode:"#FF00FF"},
+  {libelle:"Mauve ancien",htmlCode:"#FAB0FF"},
+  {libelle:"Bleue ciel",htmlCode:"#CCEEFF"},
+  {libelle:"Blanc",htmlCode:"#FFFFFF"},
+  {libelle:"Argent",htmlCode:"#C0C0C0"},
+  {libelle:"Gris",htmlCode:"#808080"},
+  {libelle:"Noir",htmlCode:"#000000"}
 ];
