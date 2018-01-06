8e1920494ef834fd18ea331324195249a4e47920
MuteManTalking <khlifihicham@orange.fr>
2018-01-06 02:24:28 +0100

    Choix par un select, choix sans passer par un bouton reset. PROBLEME : légende du graphe qui laisse les premières légende non utilisées.

--- src/app/graphiques/graphiques.component.ts
+++ src/app/graphiques/graphiques.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {Activity} from '../Classes/activity';
 import {Category} from '../Classes/category';
 import {DataService} from '../services/data.service';
-import {Color} from '../Classes/color';
-import {leave} from '@angular/core/src/profile/wtf_impl';
-import {exitCodeFromResult} from '@angular/compiler-cli';
 
 @Component({
   selector: 'app-graphiques',
@@ -14,14 +11,12 @@ import {exitCodeFromResult} from '@angular/compiler-cli';
 export class GraphiquesComponent implements OnInit {
 
   listeDurees:number[]=[];
-  listeCouleurs:Color[]=[];
   allActivity:Activity[];
   listeActivitees:string[]=[];
   allCategories:Category[];
   catego:Category;
-  name:string;
-  found:Boolean;
   dureeTotal:number=0;
+  waiter:Boolean;
   pieChartType:string = 'pie';
 
   constructor(private dataService:DataService) { }
@@ -31,26 +26,25 @@ export class GraphiquesComponent implements OnInit {
   }
 
   reset():void{
-    this.listeCouleurs=[];
-    this.listeActivitees=[];
     this.listeDurees=[];
   }
 
   selecCat(name:string):void{
-    this.found=false;
-    for(var i = 0; i<this.allCategories.length;i++){
+    this.waiter=false;
+    if(this.listeActivitees != []){
+      this.reset();
+    }
+    for(let i = 0; i<this.allCategories.length; i++){
       if(this.allCategories[i].libelle === name){
-        this.found=true;
         this.catego = this.allCategories[i];
         this.allActivity = this.dataService.getActivitiesByCategory(this.catego.id);
         if(this.allActivity.length === 0){
           alert('Aucune Activitées.');
         } else {
-          for (var j =0; j < this.allActivity.length; j++) {
+          for (let j =0; j < this.allActivity.length; j++) {
             this.dureeTotal=0;
             this.listeActivitees[j]=this.allActivity[j].nom;
-            this.listeCouleurs[j]=this.allActivity[j].category.color;
-            for(var k =0;k<this.allActivity[j].mesDurees.length;k++){
+            for(let k =0; k<this.allActivity[j].mesDurees.length; k++){
               this.dureeTotal += this.allActivity[j].mesDurees[k].secondsPassed;
             }
             this.listeDurees[j] = this.dureeTotal;
@@ -58,8 +52,6 @@ export class GraphiquesComponent implements OnInit {
         }
       }
     }
-    if(this.found === false){
-      alert('Categorie inconnue.');
-    }
+    this.waiter=true;
   }
 }
