0ad78a8cf369a33f92f0d92fb9e1caef1e608a49
MuteManTalking <khlifihicham@orange.fr>
2018-01-09 20:14:44 +0100

    Code plus propre

--- src/app/graphiques/graphiques.component.ts
+++ src/app/graphiques/graphiques.component.ts
@@ -45,61 +45,41 @@ export class GraphiquesComponent implements OnInit {
   }
 
 
-  getDurees(name:Category):void {
-
-    setTimeout(() => {
-      this.allActivity2 = this.dataService.getActivitiesByCategory(name.id);
-      //--- Recupération de toutes les Dates de toutes les activitées d'un catégories ---//
-      let cpt = 0;
-      for (let t = 0; t < this.allActivity2.length; t++) {
-        this.listeActivitees[t] = this.allActivity2[t].nom;
-        for (let u = 0; u < this.allActivity2[t].mesDurees.length; u++) {
-          this.tempsStart[cpt] = this.allActivity2[t].mesDurees[u].start;
-          this.tempsEnd[cpt] = this.allActivity2[t].mesDurees[u].end;
-          cpt++;
-        }
+  creatingList(name:Category):void {
+    this.allActivity2 = this.dataService.getActivitiesByCategory(name.id);
+    let cpt = 0;
+    for (let t = 0; t < this.allActivity2.length; t++) {
+      this.listeActivitees[t] = this.allActivity2[t].nom;
+      for (let u = 0; u < this.allActivity2[t].mesDurees.length; u++) {
+        this.tempsStart[cpt] = this.allActivity2[t].mesDurees[u].start;
+        cpt++;
       }
-
-      //--- Trie des tableaux de Dates ---//
-      this.tempsStart.sort((date1, date2) => {
-        if (date1.getFullYear() < date2.getFullYear()) {
-          return -1;
-        } else if (date1.getMonth() < date2.getMonth()) {
-          return -1;
-        } else if (date1.getMonth() === date2.getMonth()) {
-          if (date1.getDay() < date2.getDay()) {
-            return -1;
-          } else if(date1.getDay()===date2.getDay()){
-            if(date1.getHours()<date2.getHours()){
-              return -1;
-            }
-          }
-        }
-        return 1;
-      });
-
-      //--- Selection de seulement des jours unique ---//
-      let cpt2=0;
-      this.tempsStartF[cpt2]=this.tempsStart[cpt2];
-      cpt2++;
-      for(let h=1;h<this.tempsStart.length;h++){
-        if(this.tempsStartF[cpt2-1].getDay()!=this.tempsStart[h].getDay()) {
-          this.tempsStartF[cpt2] = this.tempsStart[h];
-          cpt2++;
-        }
-        if(this.tempsStartF[cpt2-1].getDay()==this.tempsStart[h].getDay()){
-          if(this.tempsStartF[cpt2-1].getMonth()!=this.tempsStart[h].getMonth()){
-            this.tempsStartF[cpt2] = this.tempsStart[h];
-            cpt2++;
-          }
-        }
-      }
-    }, 7);
+    }
+    GraphiquesComponent.trieTableau(this.tempsStart);
+    GraphiquesComponent.trieTableauDoublonJ(this.tempsStart,this.tempsStartF);
   }
 
-  selecCat():void {
-    setTimeout(()=>{
-      this.reset();
+  tempsDebSelected():void {
+    let cpt = 0;
+    for (let t = 0; t < this.allActivity2.length; t++) {
+      for (let u = 0; u < this.allActivity2[t].mesDurees.length; u++) {
+        this.tempsStart[cpt] = this.allActivity2[t].mesDurees[u].start;
+        this.tempsEnd[cpt] = this.allActivity2[t].mesDurees[u].end;
+        cpt++;
+      }
+    }
+    GraphiquesComponent.trieTableau(this.tempsEnd);
+    GraphiquesComponent.trieTableauDoublonJ(this.tempsEnd,this.tempsEndF);
+    cpt=0;
+    for(let i=0;i<this.tempsEndF.length;i++){
+      if(this.tempsDeb.getDay()<=this.tempsEndF[i].getDay()){
+        this.tempsEndFSup[cpt] = this.tempsEndF[i];
+        cpt++;
+      }
+    }
+  }
+
+  tempsFinSelected():void {
       for (let j = 0; j < this.allActivity2.length; j++) {
         this.dureeTotal = 0;
         for (let k = 0; k < this.allActivity2[j].mesDurees.length; k++) {
@@ -110,58 +90,10 @@ export class GraphiquesComponent implements OnInit {
         this.listeDurees[j] = this.dureeTotal;
         this.dureeTOTAL += this.dureeTotal;
       }
-      //--- Affichage des pourcentages---//
+
       for (let m = 0; m < this.listeDurees.length; m++) {
         this.percentage[m] = Math.round((this.listeDurees[m] / this.dureeTOTAL) * 100 * 100) / 100;
       }
-    },5);
-  }
-
-  tempsDebSelected():void{
-    //setTimeout(()=>{
-      if(this.tempsEndFSup != []) {
-        this.tempsEndFSup=[];
-        this.tempsFin=null;
-      }
-      this.tempsEnd.sort((date1, date2) => {
-        if (date1.getFullYear() < date2.getFullYear()) {
-          return -1;
-        } else if (date1.getMonth() < date2.getMonth()) {
-          return -1;
-        } else if (date1.getMonth() === date2.getMonth()) {
-          if (date1.getDay() < date2.getDay()) {
-            return -1;
-          } else if(date1.getDay()===date2.getDay()){
-            if(date1.getHours()<date2.getHours()){
-              return -1;
-            }
-          }
-        }
-        return 1;
-      });
-      let cpt2=1;
-      this.tempsEndF[0]=this.tempsEnd[0];
-      for(let h=1;h<this.tempsEnd.length;h++){
-        if(this.tempsEndF[cpt2-1].getDay()!=this.tempsEnd[h].getDay()) {
-          this.tempsEndF[cpt2] = this.tempsEnd[h];
-          cpt2++;
-        }
-        if(this.tempsEndF[cpt2-1].getDay()==this.tempsEnd[h].getDay()) {
-          if (this.tempsEndF[cpt2 - 1].getMonth() != this.tempsEnd[h].getMonth()) {
-            this.tempsEndF[cpt2] = this.tempsEnd[h];
-            cpt2++;
-          }
-        }
-      }
-      let cpt=0;
-      for(let i=0;i<this.tempsEndF.length;i++){
-        if(this.tempsDeb.getDay()<=this.tempsEndF[i].getDay()){
-            this.tempsEndFSup[cpt] = this.tempsEndF[i];
-            cpt++;
-        }
-      }
-
-    //},10)
   }
 
   waitTempsDeb():void{
@@ -169,21 +101,53 @@ export class GraphiquesComponent implements OnInit {
       this.tempsDebSelected();
     }
   }
+
   waitBoth():void{
     if(this.tempsDeb != null && this.tempsFin !=null && this.catego!=null){
-      this.selecCat();
+      this.tempsFinSelected();
     }
   }
+
   waitCatego():void {
-    this.tempsStart = [];
-    this.tempsEnd = [];
-    this.tempsDeb=null;
-    this.tempsFin=null;
-    this.tempsStartF=[];
-    this.tempsEndF = [];
-    this.tempsEndFSup=[];
     if (this.catego != null) {
-      this.getDurees(this.catego);
+      this.creatingList(this.catego);
+    }
+  }
+
+  static trieTableau(Tab:Date[]):void{
+    Tab.sort((date1, date2) => {
+      if (date1.getFullYear() < date2.getFullYear()) {
+        return -1;
+      } else if (date1.getMonth() < date2.getMonth()) {
+        return -1;
+      } else if (date1.getMonth() === date2.getMonth()) {
+        if (date1.getDay() < date2.getDay()) {
+          return -1;
+        } else if(date1.getDay()===date2.getDay()){
+          if(date1.getHours()<date2.getHours()){
+            return -1;
+          }
+        }
+      }
+      return 1;
+    });
+  }
+
+  static trieTableauDoublonJ(Tab:Date[], Tab1:Date[]):void{
+    let cpt2=0;
+    Tab1[cpt2]=Tab[cpt2];
+    cpt2++;
+    for(let h=1;h<Tab.length;h++){
+      if(Tab1[cpt2-1].getDay()!=Tab[h].getDay()) {
+        Tab1[cpt2] = Tab[h];
+        cpt2++;
+      }
+      if(Tab1[cpt2-1].getDay()==Tab[h].getDay()){
+        if(Tab1[cpt2-1].getMonth()!=Tab[h].getMonth()){
+          Tab1[cpt2] = Tab[h];
+          cpt2++;
+        }
+      }
     }
   }
 }
